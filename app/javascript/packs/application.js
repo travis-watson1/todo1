695090bee422689ff9aad32197b80d7632e46ff0
Travis Watson <watsonjamest@gmail.com>
2019-08-08 11:04:25 -0500

    Refactor code for Rails 6

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
+require("packs/tasks.js")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
