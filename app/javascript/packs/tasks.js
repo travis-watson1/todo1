e39e85ccd0e7a40e24634d72a395179f55b00a63
Travis Watson <watsonjamest@gmail.com>
2019-08-08 11:11:55 -0500

    Update design of tasks page

--- app/javascript/packs/tasks.js
+++ app/javascript/packs/tasks.js
@@ -65,6 +65,7 @@ $(function () {
             var ulTodos = $('.todo-list');
             ulTodos.append(htmlString);
             $('.toggle').click(toggleTask);
+            $('.new-todo').val('');
         });
     });
 });
